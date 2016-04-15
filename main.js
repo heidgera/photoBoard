2947cce7084b1855d3ee6d8e3480c578fc198dfe
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2016-04-15 16:53:35 -0500

    tune the screen

--- main.js
+++ main.js
@@ -35,6 +35,8 @@ app.on('ready', function() {
     height: size.height,
   });
 
+  mainWindow.setMenu(null);
+
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
