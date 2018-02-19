776e513b89ba4137f6f297207836cf31145e1482
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-02-19 14:31:25 -0600

    add quit shortcut

--- ForBoot/install.sh
+++ ForBoot/install.sh
@@ -9,6 +9,10 @@ curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo apt-get install --no-install-recommends hostapd dnsmasq pigpio network-manager xserver-xorg xinit xserver-xorg-video-fbdev xserver-xorg-video-fbturbo libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
 
+sudo apt-get install libx11-dev libxtst-dev libxt-dev libx11-xcb-dev
+
+sudo apt-get install libxkbcommon-dev libxkbcommon-x11-dev
+
 echo "\nClone the application"
 
 git clone https://github.com/${ACCOUNT}/${REPO_NAME}.git
