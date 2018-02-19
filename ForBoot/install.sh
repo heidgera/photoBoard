bf8fa919f658be7a205df42a0ef7450cef817ae3
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-02-19 15:12:32 -0600

    add quit shortcut

--- ForBoot/install.sh
+++ ForBoot/install.sh
@@ -3,34 +3,40 @@
 REPO_NAME=photoBoard
 ACCOUNT=heidgera
 
-echo "\nInstalling node:"
+echo -e "\nInstalling node:"
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo apt-get install --no-install-recommends hostapd dnsmasq pigpio network-manager xserver-xorg xinit xserver-xorg-video-fbdev xserver-xorg-video-fbturbo libxss1 libgconf-2-4 libnss3 git nodejs libgtk2.0-0 libxtst6
 
-sudo apt-get install libx11-dev libxtst-dev libxt-dev libx11-xcb-dev
+#sudo apt-get install libx11-dev libxtst-dev libxt-dev libx11-xcb-dev
 
-sudo apt-get install libxkbcommon-dev libxkbcommon-x11-dev
+#sudo apt-get install libxkbcommon-dev libxkbcommon-x11-dev
 
-echo "\nClone the application"
+#sudo apt-get install cmake automake libtool pkg-config
+
+echo -e "\nClone the application"
 
 git clone https://github.com/${ACCOUNT}/${REPO_NAME}.git
 
 cd ${REPO_NAME}
 
-echo "\nInit the submodules:"
+echo -e "\nInit the submodules:"
 
 git submodule init
 
 git submodule update
 
-echo "\nInstalling dependencies for application:"
+echo -e "\nInstalling dependencies for application:"
 
 npm i
 
-echo "\nConfiguring"
+echo -e "\nConfiguring"
 
 cd piFig
+
+mkdir node_modules
+
+
 
 sudo node install.js
