da6b2e5e9c48ddc03f4fe5ac15400efb6ecab09d
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-02-21 15:33:43 -0600

    fix server locations

--- local/src/app.js
+++ local/src/app.js
@@ -4,7 +4,7 @@ var remote = require('electron').remote;
 
 var process = remote.process;
 
-//remote.getCurrentWindow().closeDevTools();
+remote.getCurrentWindow().closeDevTools();
 
 window.appDataDir = '/Users/aheidgerken';
 
